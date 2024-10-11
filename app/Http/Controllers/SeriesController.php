1368313e29e76fd7596f4188186f01be3a92cfa1
Daniel Corbellini <daniel.corbellini28@gmail.com>
2024-10-11 14:57:00 -0300

    Making the CRUD and new models

--- app/Http/Controllers/SeriesController.php
+++ app/Http/Controllers/SeriesController.php
@@ -5,15 +5,19 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use App\Http\Controllers\Controller;
+use App\Http\Requests\SeriesFormRequest;
 use App\Models\Serie;
+use Psy\Command\EditCommand;
 
 class SeriesController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
         $series = Serie::query()->orderBy('name', 'desc')->get();
+        $mensagemSucesso = session('mensagem.sucesso');
 
-        return view('series.index')->with('series', $series);
+        return view('series.index')
+            ->with('series', $series)->with('mensagemSucesso', $mensagemSucesso);
     }
 
     public function create()
@@ -21,13 +25,32 @@ class SeriesController extends Controller
         return view('series.create');
     }
 
-    public function store(Request $request)
+    public function store(SeriesFormRequest $request)
     {
-        $nomeSerie = $request->input('nome');
-        $serie = new Serie();
-        $serie->name = $nomeSerie;
-        $serie->save();
 
-        return redirect('/series');
+        Serie::create($request->all());
+        return to_route('series.index')
+            ->with('mensagem.sucesso', "Série '{$request->name}' adicionada com sucesso");
+    }
+
+    public function destroy(Serie $series)
+    {
+        $series->delete();
+        return to_route('series.index')
+            ->with("mensagem.sucesso", "Série '$series->name' removida com sucesso");
+    }
+
+    public function update(Request $request, SeriesFormRequest $series)
+    {
+        $series->fill($request->all());
+        $series->save();
+        return to_route('series.index')
+            ->with('mensagem.sucesso', "Série {$series->name} atualizada com sucesso!");
+    }
+
+    public function edit(Serie $series)
+    {
+        dd($series->season);
+        return view('series.edit')->with('serie', $series);
     }
 }
--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -4,15 +4,27 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="{{asset('css/app.css')}}">
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     <title>{{ $title }} - Controle de Séries</title>
 </head>
 
 <body>
     <div class="container">
 
-        <h1>{{$title}}</h1>
+        <h1>{{ $title }}</h1>
+
+        @if ($errors->any())
+            <div class="alert alert-danger">
+                <ul>
+                    @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
+            </div>
+        @endif
+
         {{ $slot }}
     </div>
 </body>
+
 </html>
